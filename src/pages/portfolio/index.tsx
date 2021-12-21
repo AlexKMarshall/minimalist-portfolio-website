9d2e2e634253e2b94f7ff01de7936bb429f1ec50
Alex Marshall <alex.k.marshall83@gmail.com>
2021-12-21 15:39:37 +0000

    add portfolio page;

---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,98 @@
+import * as styles from 'src/styles/page.css'
+
+import { Heading, Layout, Link } from 'src/components'
+
+import Head from 'next/head'
+import Image from 'next/image'
+import type { NextPage } from 'next'
+import bookmarkImage from 'public/portfolio/desktop/image-portfolio-bookmark@2x.jpg'
+import fyloImage from 'public/portfolio/desktop/image-portfolio-fylo@2x.jpg'
+import insureImage from 'public/portfolio/desktop/image-portfolio-insure@2x.jpg'
+import manageImage from 'public/portfolio/desktop/image-portfolio-manage@2x.jpg'
+
+const projects: ProjectArticleProps[] = [
+  {
+    image: manageImage,
+    heading: 'Manage',
+    textContent: `This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.`,
+    href: '/portfolio/manage',
+  },
+  {
+    image: bookmarkImage,
+    heading: 'Bookmark',
+    textContent: `  This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the features section.`,
+    href: '/portfolio/bookmark',
+  },
+  {
+    image: insureImage,
+    heading: 'Insure',
+    textContent: `This was a small project which mostly consisted of HTML and CSS. I built a fully-responsive landing page. The only JavaScript this project required was to enable the toggling of the mobile navigation.`,
+    href: '/portfolio/insure',
+  },
+  {
+    image: fyloImage,
+    heading: 'Fylo',
+    textContent: `This project was built in pure HTML and CSS. I had mobile and desktop designs to work to and built it so that it was fully-responsive. I took a mobile-first approach and used modern CSS like Flexbox and Grid for layout purposes.`,
+    href: '/portfolio/fylo',
+  },
+]
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Alex Spencer - Web Developer - Portfolio</title>
+        <meta
+          name="description"
+          content="Portfolio of Alex Spencer, Web Developer"
+        />
+        <link rel="icon" href="/favicon-32x32.png" />
+      </Head>
+
+      <Layout>
+        <div className={styles.enourmousStack}>
+          {projects.map((project) => (
+            <ProjectArticle key={project.heading} {...project} />
+          ))}
+        </div>
+      </Layout>
+    </>
+  )
+}
+
+type ProjectArticleProps = {
+  image: StaticImageData
+  heading: string
+  textContent: string
+  href: string
+}
+function ProjectArticle({
+  image,
+  heading,
+  textContent,
+  href,
+}: ProjectArticleProps): JSX.Element {
+  return (
+    <article className={styles.article}>
+      <Image
+        src={image}
+        alt=""
+        placeholder="blur"
+        objectFit="cover"
+        objectPosition="75% center"
+      />
+      <div className={styles.aboutContent}>
+        <Heading level={2} size={1}>
+          {heading}
+        </Heading>
+
+        <p>{textContent}</p>
+        <Link href={href} type="secondary">
+          View Project
+        </Link>
+      </div>
+    </article>
+  )
+}
+
+export default Home
